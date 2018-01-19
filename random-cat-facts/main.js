48604d79db4cca9ad236e59ff2d8dcefaf10a10c
mmlumba <mmlumba@gmail.com>
2018-01-19 11:47:32 -0500

    js answers

---
+++ random-cat-facts/main.js
@@ -0,0 +1,17 @@
+var displayFact = document.getElementById("cat-fact");
+
+var CatFacts = {
+  '1': "The ancient Egyptians were the first to tame the cat (in about 3000 BC), and used them to control pests.",
+  '2': "Cats share 95.6% of their DNA with tigers.",
+  '3': "Cats are asleep for 70% of their lives.",
+  '4': "Cat kidneys are super efficient, they can rehydrate by drinking seawater.",
+  '5': "Kittens who are taken along on short, trouble-free car trips to town tend to make good passengers when they get older. They get used to the sounds and motions of traveling and make less connection between the car and the visits to the vet."
+}
+
+var factNumber = Math.floor(Math.random()*5)+1;
+
+function randomFact(){
+  return CatFacts[factNumber];
+}
+
+displayFact.textContent = randomFact();
