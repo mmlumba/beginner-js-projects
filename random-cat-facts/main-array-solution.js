2a8c5d70b366a3cc195a8e1a15ab6d487667db49
mmlumba <mmlumba@gmail.com>
2018-01-21 12:16:45 -0500

    solution with arrays

---
+++ random-cat-facts/main-array-solution.js
@@ -0,0 +1,19 @@
+var factPlaceholder = document.getElementById("cat-fact");
+var showFact = document.getElementById("show-fact");
+
+var CatFacts = ["The ancient Egyptians were the first to tame the cat (in about 3000 BC), and used them to control pests.",
+               "Cats share 95.6% of their DNA with tigers.",
+               "Cats are asleep for 70% of their lives.",
+               "Cat kidneys are super efficient, they can rehydrate by drinking seawater.",
+               "Kittens who are taken along on short, trouble-free car trips to town tend to make good passengers when they get older. They get used to the sounds and motions of traveling and make less connection between the car and the visits to the vet."]
+
+var factNumber;
+
+function randomFact(){
+  return CatFacts[factNumber];
+}
+
+showFact.addEventListener('click', function(){
+  factNumber = Math.floor(Math.random()*5);
+  factPlaceholder.textContent = randomFact();
+});
